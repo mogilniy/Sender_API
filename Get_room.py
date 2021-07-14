e8abfbfcfc7da06f854c657312f518cb964eed8d
mogilniy <g.mogilniy@gmail.com>
2021-07-14 19:47:40 +0300

    remove old version

--- Get_room.py
+++
@@ -1,9 +0,0 @@
-import sys
-# use 1 param -key_webex
-# print to console list room
-if __name__ == "__main__":
-    i=len(sys.argv)
-    ii=1
-    while ii<i:
-        print(sys.argv[ii])
-        ii=ii+1
