d0d122a23e1fd01e8167ea8d853b25ea1a1f0bf7
mogilniy <g.mogilniy@gmail.com>
2021-07-14 20:11:26 +0300

    itog vresion list_room

---
+++ Get_room.py
@@ -0,0 +1,50 @@
+import urllib.parse
+import time
+import requests
+import json
+import sys
+
+apiUri="https://webexapis.com/v1/rooms"
+
+#accessToken="NzMyODZiMTMtZjU3NS00NjlkLWFmYzMtNTllNmZkOTM2MDBhMjNiZjI4NTQtMDIx_PE93_bed4d195-92fc-4dd3-946a-c5b317a8386e"
+accessToken=sys.argv[1]
+#print(accessToken)
+#access_token = 'your_token_here'  
+url = 'https://webexapis.com/v1/rooms'
+headers = {
+    'Authorization': 'Bearer {}'.format(accessToken),
+    'Content-Type': 'application/json'
+}
+params={'max': '100'}
+r = requests.get(url, headers=headers, params=params)
+
+
+
+
+
+'''
+r=requests.get(apiUri,
+                headers={'Token':accessToken}
+              )
+'''
+json_data = r.json()
+
+def name_all_group():
+  noe=len(json_data["items"])
+  name_group=[]
+  n=0
+  while True:
+    if n<noe:
+      x=json_data["items"][n]["title"]
+      name_group.append(x) 
+    else:
+      break
+    n=n+1
+  return name_group
+temp=name_all_group()
+intl=len(temp)
+i=0
+ii=0
+while ii<intl:
+      print(temp[ii])
+      ii=ii+1
