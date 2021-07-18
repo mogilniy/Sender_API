e3d1c67fe6164c94e97e98dd64192131fedbfcfe
NetAcad DEVASC <changeme@example.com>
2021-07-18 19:02:59 +0000

    edit

--- Get_room.py
+++ Get_room.py
@@ -9,7 +9,8 @@ apiUri="https://webexapis.com/v1/rooms"
 #accessToken=""
 accessToken=sys.argv[1]
 #print(accessToken)
-#access_token = 'your_token_here'  
+
+ 
 url = 'https://webexapis.com/v1/rooms'
 headers = {
     'Authorization': 'Bearer {}'.format(accessToken),
@@ -18,15 +19,6 @@ headers = {
 params={'max': '100'}
 r = requests.get(url, headers=headers, params=params)
 
-
-
-
-
-'''
-r=requests.get(apiUri,
-                headers={'Token':accessToken}
-              )
-'''
 json_data = r.json()
 
 def name_all_group():
@@ -43,7 +35,7 @@ def name_all_group():
   return name_group
 temp=name_all_group()
 intl=len(temp)
-i=0
+
 ii=0
 while ii<intl:
       print(temp[ii])
